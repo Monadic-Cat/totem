ebfed70cfe60e8fd62905eb78ae3206c35a53a2c
Monadic Cat <greenmonadcat@gmail.com>
2019-09-11 22:55:39 -0500

    Preliminary dice, no command framework

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 extern crate serenity;
 
+use mice::prelude::*;
 use serenity::client::{Client, Context, EventHandler};
-use serenity::model::gateway::Ready;
+use serenity::model::{channel::Message, gateway::Ready};
 use std::env;
 
 struct Totem;
 
+const ROLLP: &str = "!roll ";
+
 impl EventHandler for Totem {
 	fn ready(&self, _: Context, _: Ready) {
 		println!("I'm alive!");
+	}
+	fn message(&self, ctx: Context, msg: Message) {
+		if msg.content.starts_with(ROLLP) {
+			match msg.channel_id.say(
+				&ctx.http,
+				match roll(&msg.content[ROLLP.len()..]) {
+					Ok(x) => x.format(MiceFormat::new().total_right()),
+					Err(x) => format!("{}", x),
+				},
+			) {
+				Ok(_) => (),
+				Err(x) => eprintln!("{}", x),
+			}
+		}
 	}
 }
 
